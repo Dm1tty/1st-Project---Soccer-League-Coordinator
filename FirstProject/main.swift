23c2134de1df45ce4a070d345a0fab009d239b61
Dzmitry <ma.dzmitry@gmail.com>
2019-05-21 14:30:24 -0700

    Initial Commit

---
+++ FirstProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FirstProject
+//
+//  Created by Dzmitry Matsiulka on 5/21/19.
+//  Copyright © 2019 Dzmitry Matsiulka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
